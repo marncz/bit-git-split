b38bda7c8911d958ea50986e24ea024e0a1456fd
marncz <M.Czarnecki1@uni.brighton.ac.uk>
2017-02-17 21:28:34 +0000

    Testing contributors

--- hello-world.php
+++ hello-world.php
@@ -3,5 +3,5 @@
 include_once("./src/include.php");
 
 echo blockchain_config("api_key");
-
+print_r( get_contributors() );
 ?>
--- src/functions/github.php
+++ src/functions/github.php
@@ -1,12 +1,27 @@
 <?php
 include_once('./src/functions/config.php');
 
+/*
+
+Call GitHub and gather contributors along with their Bitcoin addresses
+
+@return array
+*/
 function get_contributors (){
 
-  $bitcoin_md_url = 
-  $homepage = file_get_contents('http://www.example.com/');
-  echo $homepage;
+  $bitcoin_txt_url = "https://raw.githubusercontent.com/". main_config("repo_name") ."/master/BITCOIN.txt";
+  $html_response = file_get_contents($bitcoin_txt_url);
+  $contributors = explode(PHP_EOL, $html_response);
 
+  $final = array();
+
+  foreach ($contributors as $contributor)
+  {
+    $ex = explode(":", $contributor);
+    $final[ $ex[0] ] = $ex[1];
+  }
+
+  return $final;
 }
 
 
